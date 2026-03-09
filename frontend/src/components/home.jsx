31e7c3d4df58a7720a2540aaf74ffaa565ef4e85
KARAN YADAV <karanyadav@KARANs-MacBook-Air.local>
2026-03-09 16:42:45 +0530

    photo

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './home.css'
 import navbar from '../assets/ashi.png'
 import bag from '../assets/page.jpeg'
-import collage from '../assets/study.jpg'
+import collage from '../assets/image.png'
 import { Link } from "react-router-dom";
 
 const Contact = () => {
